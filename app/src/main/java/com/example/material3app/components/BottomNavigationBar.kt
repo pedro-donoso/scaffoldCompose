5deb7adb0f00f2e922e68717268ef40eb33a4aa1
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-12 16:18:59 -0300

    agrego NavigationBar

---
+++ app/src/main/java/com/example/material3app/components/BottomNavigationBar.kt
@@ -0,0 +1,64 @@
+package com.example.material3app.components
+
+import android.icu.text.CaseMap.Title
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun BottomNavigationBar() {
+
+    val navigationList = listOf(
+        NavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home
+        ),
+        NavigationItem(
+            title = "Favorites",
+            selectedIcon = Icons.Filled.Favorite,
+            unselectedIcon = Icons.Outlined.FavoriteBorder
+        ),
+        NavigationItem(
+            title = "Notifications",
+            selectedIcon = Icons.Filled.Notifications,
+            unselectedIcon = Icons.Outlined.Notifications
+        ),
+        NavigationItem(
+            title = "Settings",
+            selectedIcon = Icons.Filled.Settings,
+            unselectedIcon = Icons.Outlined.Settings
+        )
+    )
+
+    NavigationBar {
+        navigationList.forEachIndexed { index, navigationItem ->
+            NavigationBarItem(
+                label = {
+                    Text(text = navigationItem.title)
+                },
+                selected = false,
+                onClick = { },
+                icon = { }
+            )
+        }
+    }
+}
+
+data class NavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
