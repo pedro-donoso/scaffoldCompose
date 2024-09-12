9a319f1458047131438fbbf6b604f4066aca9903
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-11 23:26:31 -0300

    cambio colores barra navegacion

--- app/src/main/java/com/example/material3app/MainActivity.kt
+++ app/src/main/java/com/example/material3app/MainActivity.kt
@@ -8,10 +8,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.material3app.ui.theme.Material3AppTheme
 
 //extiende de ComponentActivity para poder usar el setContent
@@ -26,12 +30,18 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     //barra de navegacion
                     topBar = {
-                        TopAppBar(title = {
-                                //titulo de la barra de navegacion
-                                Text(text = "Material 3 App")
-                            })
+                        TopAppBar(
+                            //colores de la barra de navegacion
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = Color.Blue,
+                                titleContentColor = Color.White
+                            ),
+                            title = {
+                            //titulo de la barra de navegacion
+                            Text(text = "Material 3 App")
+                        })
                     }
-                ) {paddingValues ->
+                ) { paddingValues ->
                     //contenido de la pantalla
                     Box(
                         //agrega espacio alrededor del contenido
