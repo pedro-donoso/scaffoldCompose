07c86785eba0e1596536c87cdcb8ccc72cbf0da5
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-12 00:44:03 -0300

    Agrego Card Dog

--- app/src/main/java/com/example/material3app/MainActivity.kt
+++ app/src/main/java/com/example/material3app/MainActivity.kt
@@ -5,9 +5,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.AccountCircle
@@ -18,6 +23,8 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -28,7 +35,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.material3app.ui.theme.Material3AppTheme
 
 //extiende de ComponentActivity para poder usar el setContent
@@ -63,12 +72,13 @@ class MainActivity : ComponentActivity() {
                         //agrega espacio alrededor del contenido
                         modifier = Modifier.padding(paddingValues)
                     ) {
-                        Column {
-                            for (i in 1..10) {
-                                Card(onClick = { }) {
-                                    Text(text = "Card number $i")
-                                }
-                            }
+                        Column(
+                            verticalArrangement = Arrangement.Top,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier
+                                .padding(vertical = 10.dp)
+                        ) {
+                            MaterialDogCard()
                         }
                     }
                 }
@@ -116,6 +126,27 @@ fun Material3TopAppBar() {
             Text(text = "Mi Aplicacion")
         }
     )
+}
+
+@Composable
+fun MaterialDogCard() {
+    Card(onClick = { },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            contentColor = MaterialTheme.colorScheme.onSecondaryContainer
+        ),
+        modifier = Modifier
+            .padding(horizontal = 10.dp, vertical = 15.dp)
+        ) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+        ) {
+            Column {
+                Text(text = "This is my Dog")
+            }
+        }
+    }
 }
 
 @Composable
