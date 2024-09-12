ef5876ce2716f04bd8206236a15d3a6cb30dbd14
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-11 23:23:23 -0300

    comento codigo

--- app/src/main/java/com/example/material3app/MainActivity.kt
+++ app/src/main/java/com/example/material3app/MainActivity.kt
@@ -1,18 +1,43 @@
 package com.example.material3app
 
+//importaciones necesarias
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Modifier
 import com.example.material3app.ui.theme.Material3AppTheme
 
+//extiende de ComponentActivity para poder usar el setContent
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Material3AppTheme {
-
+                //estructura de la pantalla
+                Scaffold(
+                    //barra de navegacion
+                    topBar = {
+                        TopAppBar(title = {
+                                //titulo de la barra de navegacion
+                                Text(text = "Material 3 App")
+                            })
+                    }
+                ) {paddingValues ->
+                    //contenido de la pantalla
+                    Box(
+                        //agrega espacio alrededor del contenido
+                        modifier = Modifier.padding(paddingValues)
+                    )
+                }
             }
         }
     }
