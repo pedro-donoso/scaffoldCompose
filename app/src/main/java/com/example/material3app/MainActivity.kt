edc6c4f4bcfa38d2517600c79f484718c7ab7850
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-11 23:38:36 -0300

    agrego IconButton

--- app/src/main/java/com/example/material3app/MainActivity.kt
+++ app/src/main/java/com/example/material3app/MainActivity.kt
@@ -7,7 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -32,9 +37,18 @@ class MainActivity : ComponentActivity() {
                     topBar = {
                         TopAppBar(
                             //colores de la barra de navegacion
+                            navigationIcon = {
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                        contentDescription = null
+                                    )
+                                }
+                            },
                             colors = TopAppBarDefaults.topAppBarColors(
                                 containerColor = MaterialTheme.colorScheme.primary,
-                                titleContentColor = MaterialTheme.colorScheme.onPrimary
+                                titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                                navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
                             ),
                             title = {
                                 //titulo de la barra de navegacion
