c2754dd52f054d0c17603496d23af99e850a7ea1
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-13 14:05:22 -0300

    funcionando

--- app/src/main/java/com/example/material3app/components/FavoritesScreen.kt
+++ app/src/main/java/com/example/material3app/components/FavoritesScreen.kt
@@ -2,16 +2,12 @@ package com.example.material3app.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
@@ -45,8 +41,8 @@ fun FavoriteScreen() {
     val image7 =
     "https://images.unsplash.com/photo-1561037404-61cd46aa615b?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
-    var dogList = remember {
-        mutableStateListOf<Dog>(
+    val dogList = remember {
+        mutableStateListOf(
             Dog(
                 "Toto",
                 image1
